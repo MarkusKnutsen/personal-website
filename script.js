c4842550d602ccc2ae2c6f85fe3c949864abb400
Markus Knutsen <56341751+MarkusKnutsen@users.noreply.github.com>
2025-02-15 16:25:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("header").appendChild(themeToggle);
 
     themeToggle.addEventListener("click", () => {
-        document.body.classList.toggle("theme-toggle");
-        localStorage.setItem("theme", document.body.classList.contains("theme-toggle") ? "light" : "dark");
+        document.body.classList.toggle("theme-change");
+        localStorage.setItem("theme", document.body.classList.contains("theme-change") ? "theme-change" : "dark");
     });
 
     // Load saved theme preference
-    if (localStorage.getItem("theme") === "light") {
-        document.body.classList.add("theme-toggle");
+    if (localStorage.getItem("theme") === "theme-change") {
+        document.body.classList.add("theme-change");
     }
 });
+
